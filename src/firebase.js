57b4b4acbdc580bd2715e96c4eb88d0965ef0992
Nayak-Prakhar <prakharnayak477@gmail.com>
2025-06-01 13:32:19 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue, remove } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import { getDatabase, ref, set, onValue, remove } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAg_txH-UmG4WBlYRcEti3yvWnpnT0tPGc",
   authDomain: "hotel-satyam.firebaseapp.com",
   databaseURL: "https://hotel-satyam-default-rtdb.firebaseio.com",
   projectId: "hotel-satyam",
-  storageBucket: "hotel-satyam.firebasestorage.app",
+  storageBucket: "hotel-satyam.appspot.com",
   messagingSenderId: "1055875558521",
   appId: "1:1055875558521:web:afd8661ee1bbe2b93fb8a8",
   measurementId: "G-4TLC52X22T"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const auth = getAuth(app);
-
-export { db, ref, set, onValue, remove, auth };
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export { ref, set, onValue, remove };
